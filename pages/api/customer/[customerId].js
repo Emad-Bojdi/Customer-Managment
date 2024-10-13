073b7938e23ef82e0512003c3611c304e1188381
Emaad <emadfredev@gmail.com>
2024-10-13 11:28:04 +0330

    Creted a api for getting user

---
+++ pages/api/customer/[customerId].js
@@ -0,0 +1,25 @@
+import Customer from "../../../models/Customer";
+import connectDB from "../../../utils/connectDB";
+
+
+async function handler(req, res) {
+    try {
+        await connectDB();
+    } catch (err) {
+        console.log(err);
+        res.status(200).json({ status: "success", message: "Error in connecting to database" });
+        return;
+    }
+
+    if (req.method === "GET") {
+        const id = req.query.customerId;
+        try {
+            const customer = await Customer.findById(id);
+
+        } catch (error) {
+            res.status(500).json({ status: "failed", message: "Error in retrieving data from database" })
+        }
+    }
+}
+
+export default handler;
