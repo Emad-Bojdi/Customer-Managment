b1f9e8431d9f38589b0d1d6fe3e317cae89b4382
Emaad <emadfredev@gmail.com>
2024-10-13 12:12:42 +0330

    Created function for getting data of a specific user

---
+++ components/template/CustomerEditPage.js
@@ -0,0 +1,12 @@
+
+
+const CustomerEditPage = ({data , id}) => {
+    console.log({data , id})
+  return (
+    <div>
+      Salem!
+    </div>
+  )
+}
+
+export default CustomerEditPage
--- pages/edit/[customerId].js
+++ pages/edit/[customerId].js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import CustomerEditPage from '../../components/template/CustomerEditPage'
+import { useRouter } from 'next/router';
 
 function Index() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [data, setData] = useState(null);
+    const router = useRouter();
+    const { query: { customerId }, isReady } = router;
+    useEffect(() => {
+        if (isReady) {
+            fetch(`/api/customer/${customerId}`).then(res => res.json()).then(data => setData(data.data))
+        }
+    }, [isReady])
+
+    if (data) return <CustomerEditPage data={data} id={customerId}/>;
 }
 
 export default Index
